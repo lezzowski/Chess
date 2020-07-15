8e7de8e1f4a8fed040e622419461856269d4eae2
lezzowski <shafiyiomid@gmail.com>
2020-07-15 03:37:12 +0200

    First Commit: created Pieces, no movements yet

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import Pieces.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pawn p = new Pawn(1,1,true);
+        Pawn p1 = new Pawn(1,1,false);
+        System.out.println(p);
+        System.out.println(p1);
+        Knight k = new Knight(1,1,true);
+        Knight k1 = new Knight(1,1,false);
+        System.out.println(k);
+        System.out.println(k1);
+        Bishop b = new Bishop(1,1,true);
+        Bishop b1 = new Bishop(1,1,false);
+        System.out.println(b);
+        System.out.println(b1);
+        Queen q = new Queen(1,1,true);
+        Queen q1 = new Queen(1,1,false);
+        System.out.println(q);
+        System.out.println(q1);
+        King king = new King(1,1,true);
+        King king1 = new King(1,1,false);
+        System.out.println(king);
+        System.out.println(king1);
+    }
+}
--- src/Pieces/Pawn.java
+++ src/Pieces/Pawn.java
@@ -7,7 +7,7 @@ public class Pawn extends Piece {
     public Pawn(int x, int y, boolean whiteColor) {
         super(x, y, whiteColor);
 
-        this.setIcon(whiteColor ? "\u2659" : "\u265F");
+        this.setIcon(whiteColor ? "\u265F" : "\u2659");
     }
 
 
