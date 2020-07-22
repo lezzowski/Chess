44866a3aaf3e4cec686a1e1365763798647f6916
lezzowski <shafiyiomid@gmail.com>
2020-07-22 12:52:03 +0200

    All pieces working as intended. Still no collision

--- src/Pieces/Knight.java
+++ src/Pieces/Knight.java
@@ -10,6 +10,19 @@ public class Knight extends Piece {
 
     @Override
     public boolean CheckForMove(int xFromMove, int yFromMove, int xToMove, int yToMove) {
+        //Top moves
+        if ((xToMove == xFromMove +2) && (yToMove == yFromMove +1) ||(xToMove == xFromMove +2) && (yToMove == yFromMove -1)
+                || (xToMove == xFromMove -2) && (yToMove == yFromMove -1)
+                || (xToMove == xFromMove -2) && (yToMove == yFromMove +1)){
+            return true;
+        }
+
+        //Bottom moves
+        if ((xToMove == xFromMove +1) && (yToMove == yFromMove +2) ||(xToMove == xFromMove +1) && (yToMove == yFromMove -2)
+                || (xToMove == xFromMove -1) && (yToMove == yFromMove -2)
+                || (xToMove == xFromMove -1) && (yToMove == yFromMove +2)){
+            return true;
+        }
 
         return false;
     }
