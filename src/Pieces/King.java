62137e95710932b0a3f0a0aa98c96e9d2965b7db
lezzowski <shafiyiomid@gmail.com>
2020-07-22 12:16:53 +0200

    Bishop,Rook,Queen,King work as intended, Knight left. Still no collision

--- src/Pieces/King.java
+++ src/Pieces/King.java
@@ -2,7 +2,7 @@ package Pieces;
 
 public class King extends Piece {
 
-    private boolean firstMove;
+    private boolean firstMove = true;
 
     public King(boolean whiteColor) {
         super(whiteColor);
@@ -12,6 +12,19 @@ public class King extends Piece {
 
     @Override
     public boolean CheckForMove(int xFromMove, int yFromMove, int xToMove, int yToMove) {
+        if ((xToMove == xFromMove - 1) || (xToMove == xFromMove + 1)){
+            if ((yToMove == yFromMove -1) || (yToMove == yFromMove + 1)){
+                firstMove = false;
+                return true;
+            }
+        }
+        if (xToMove == xFromMove){
+            firstMove = false;
+            return yToMove == yFromMove + 1 || yToMove == yFromMove - 1;
+        }else if (yToMove == yFromMove){
+            firstMove = false;
+            return xToMove == xFromMove + 1 || xToMove == xFromMove - 1;
+        }
 
         return false;
     }
